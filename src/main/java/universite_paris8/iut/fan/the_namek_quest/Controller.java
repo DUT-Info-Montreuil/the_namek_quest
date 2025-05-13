a4a52d24a7484a92790a3cd6d49142e01f19098a
nourchouari <novrchouari@gmail.com>
2025-05-13 14:03:41 +0200

    Modification du FXML

--- src/main/java/universite_paris8/iut/fan/the_namek_quest/Controller.java
+++ src/main/java/universite_paris8/iut/fan/the_namek_quest/Controller.java
@@ -26,8 +26,11 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.terrain = new Terrain();
-        Image imageCiel = new Image(getClass().getResourceAsStream("/universite_paris8/iut/fan/the_namek_quest/images/ciel.png"));
+        tilePane.setPrefColumns(terrain.largeurTerrain());
+        tilePane.setPrefSize(terrain.largeurTerrain()*16,terrain.hauteurTerrain()*16);
+        Image imageCiel = new Image(getClass().getResourceAsStream("/universite_paris8/iut/fan/the_namek_quest/images/ciel.png") );
         Image imageSol = new Image(getClass().getResourceAsStream("/universite_paris8/iut/fan/the_namek_quest/images/sol.png"));
+        Image imageHerbe = new Image(getClass().getResourceAsStream("/universite_paris8/iut/fan/the_namek_quest/images/herbe.png"));
         //boucle
         for(int i = 0; i< terrain.hauteurTerrain(); i++) {
             for(int j = 0; j< terrain.largeurTerrain(); j++) {
@@ -35,11 +38,14 @@ public class Controller implements Initializable {
                     tilePane.getChildren().add(new ImageView(imageCiel));
                     System.out.println("1");
                 }
-                else{
+                else if(this.terrain.getTerrain()[i][j]==2){
                     System.out.println("2");
                     tilePane.getChildren().add(new ImageView(imageSol));
                 }
-
+                else if(this.terrain.getTerrain()[i][j]==3){
+                    System.out.println("3");
+                    tilePane.getChildren().add(new ImageView(imageHerbe));
+                }
             }
         }
     }
