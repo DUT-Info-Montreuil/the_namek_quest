a239ec076c7e44165bd81fca6630a7e993425d62
nourchouari <novrchouari@gmail.com>
2025-05-14 11:25:20 +0200

    Ajout des classes des persos et leur gestion

--- src/main/java/universite_paris8/iut/fan/the_namek_quest/model/Personnage.java
+++ src/main/java/universite_paris8/iut/fan/the_namek_quest/model/Personnage.java
@@ -1,5 +1,49 @@
 package universite_paris8.iut.fan.the_namek_quest.model;
 
-public interface Personnage {
+public abstract class Personnage {
 
+    private int pv;
+    private int x;
+    private int y;
+    private int vitesse;
+    private Terrain terrain;
+
+    public Personnage(int pv, int x, int y) {
+        this.pv = pv;
+        this.x = x;
+        this.y = y;
+    }
+
+
+    //getter
+    public int getPv() {
+        return this.pv;
+    }
+    public int getX() {
+        return this.x;
+    }
+    public int getY() {
+        return this.y;
+    }
+    public int getVitesse() {
+        return this.vitesse;
+    }
+    public Terrain getTerrain() {
+        return this.terrain;
+    }
+
+
+    //setter
+    public void setPv(int pv) {
+        this.pv = pv;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public void setVitesse(int vitesse) {
+        this.vitesse = vitesse;
+    }
 }
---
+++ src/main/java/universite_paris8/iut/fan/the_namek_quest/model/Trunks.java
@@ -0,0 +1,9 @@
+package universite_paris8.iut.fan.the_namek_quest.model;
+
+public class Trunks extends Personnage {
+
+    public Trunks() {
+        super(200, 5, 4);
+    }
+
+}
