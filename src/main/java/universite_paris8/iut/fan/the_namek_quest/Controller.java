6734eb22c68dd353e3c8c3ed76030e249179c2f6
FousseyniC <fousseynicisse45@gmail.com>
2025-05-15 11:07:33 +0200

    merge branch deplacementPersoModel

--- src/main/java/universite_paris8/iut/fan/the_namek_quest/Controller.java
+++ src/main/java/universite_paris8/iut/fan/the_namek_quest/Controller.java
@@ -1,20 +1,14 @@
 package universite_paris8.iut.fan.the_namek_quest;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
-import javafx.stage.Stage;
 import universite_paris8.iut.fan.the_namek_quest.model.Terrain;
 import universite_paris8.iut.fan.the_namek_quest.vue.TerrainVue;
 import universite_paris8.iut.fan.the_namek_quest.vue.TrunksVue;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
