c8c9752b46997f15a9a0b2fb5ab17b1eed632a54
nourchouari <novrchouari@gmail.com>
2025-05-22 23:03:16 +0300

    ajout des id pour chaque outils et armes

---
+++ src/main/java/universite_paris8/iut/fan/the_namek_quest/model/Inventaire/ressource/Arbres.java
@@ -0,0 +1,7 @@
+package universite_paris8.iut.fan.the_namek_quest.model.Inventaire.ressource;
+
+public class Arbres extends Materieau{
+    public Arbres() {
+        super(10, "arbre");
+    }
+}
---
+++ src/main/java/universite_paris8/iut/fan/the_namek_quest/model/Inventaire/ressource/Energie.java
@@ -0,0 +1,7 @@
+package universite_paris8.iut.fan.the_namek_quest.model.Inventaire.ressource;
+
+public class Energie extends Materieau{
+    public Energie() {
+        super(4, "energie");
+    }
+}
