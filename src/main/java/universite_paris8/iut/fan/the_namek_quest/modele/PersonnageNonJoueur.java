dd5fba8e98a6533d129a2556152bce0107990be9
nourchouari <novrchouari@gmail.com>
2025-06-02 15:23:43 +0200

    Debut des pnj

---
+++ src/main/java/universite_paris8/iut/fan/the_namek_quest/modele/PersonnageNonJoueur.java
@@ -0,0 +1,19 @@
+package universite_paris8.iut.fan.the_namek_quest.modele;
+
+/**
+ * Classe PersonnageNonJoueur
+ * ---------------------------
+ *
+ **/
+
+
+public class PersonnageNonJoueur  extends Personnage {
+
+    public PersonnageNonJoueur(int x, int y, Environnement env) {
+        super(x, y, env);
+    }
+
+    public void seDeplacer(){
+
+    }
+}
